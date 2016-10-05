aeaff524b5270a337481e7f3a44decde145aad98
Zach Schwartz <zischwartz@gmail.com>
2016-10-05 17:19:55 -0400

    moved things around so static server and notebook server both work

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-python -m http.server 8000 &
-jupyter notebook --NotebookApp.allow_origin='*' --no-browser --port 8888 --ip=*
+cd public && python -m http.server 8000 & jupyter notebook --NotebookApp.allow_origin='*' --no-browser --port 8888 --ip=*
